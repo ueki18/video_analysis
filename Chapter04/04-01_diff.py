5d3d098da4967eae4290aea8a0152902a192cde1
ueki18 <32954520+ueki18@users.noreply.github.com>
2026-02-26 14:39:57 +0900

    Update 04-01_diff.py

--- Chapter04/04-01_diff.py
+++ Chapter04/04-01_diff.py
@@ -2,25 +2,26 @@ import cv2
 
 cap = cv2.VideoCapture('input.mp4')
 ret, prev = cap.read()
+prev_gray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
 
 while cap.isOpened():
     ret, curr = cap.read()
     if not ret:
         break
 
-    # フレーム間の絶対差分
-    diff = cv2.absdiff(prev, curr)
-    # グレースケールに変換
-    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+    # 現在のフレームをグレースケールに変換
+    curr_gray = cv2.cvtColor(curr, cv2.COLOR_BGR2GRAY)
+    # グレースケール画像同士で差分を計算
+    diff = cv2.absdiff(prev_gray, curr_gray)
     # 二値化
-    _, thresh = cv2.threshold(gray, 25, 255, cv2.THRESH_BINARY)
-
+    _, thresh = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)
+    
     cv2.imshow('Frame Difference', thresh)
 
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
-    prev = curr  # 現在のフレームを次回の比較用に保存
+    prev_gray = curr_gray.copy() # 現在のフレームを次回の比較用に保存
 
 cap.release()
 cv2.destroyAllWindows()
