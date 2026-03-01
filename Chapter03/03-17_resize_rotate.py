15f3186e1e1762c3a07f61adf18b8da7e89ffbdb
ueki18 <32954520+ueki18@users.noreply.github.com>
2026-03-01 17:00:39 +0900

    Update 03-17_resize_rotate.py

--- Chapter03/03-17_resize_rotate.py
+++ Chapter03/03-17_resize_rotate.py
@@ -3,7 +3,8 @@ from moviepy import VideoFileClip, vfx
 clip = VideoFileClip("input.mp4")
 edited = clip.with_effects([
     vfx.Resize(height=360),  # 高さ360にリサイズ
-    vfx.Rotate(180)          # 時計回りに180度回転（上下反転）
+    vfx.Rotate(180)          # 180度回転（上下反転）
 ])
 
 edited.write_videofile("output_rotated.mp4")
+
